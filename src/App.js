dfd50031453b544e0d9c352370e043a0ff6d2e28
DjenilsonPina <97348441+DjenilsonPina@users.noreply.github.com>
2022-01-27 16:43:40 -0100

    versao final

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      
+      <body>
+	<div class="main">  	
+		<input type="checkbox" id="chk" aria-hidden="true"/>
+
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">WebNet</label>
+					<input type="text" name="txt" placeholder="User name" required=""/>
+					<input type="email" name="email" placeholder="Email" required=""/>
+					<input type="password" name="pswd" placeholder="Password" required=""/>
+          <a className="Home" href="site2/index.html" rel="noopener noreferrer">Entrar</a>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" required=""/>
+					<input type="password" name="pswd" placeholder="Password" required=""/>
+					<button>Login</button>
+				</form>
+			</div>
+	</div>
+</body>
+
+    </div>
+  );
+}
+
+export default App;
